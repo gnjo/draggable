9a809629ef5ff03d25fce6088a022a844e375067
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-04-22 21:55:23 +0900

    Create draggable.js

---
+++ draggable.js
@@ -0,0 +1,23 @@
+
+;(function(root){
+if(!root.intaract)return console.log('need intaract')
+;
+function draggable(query){
+return interact(query)
+.draggable({
+  modifiers: [
+    interact.modifiers.snap({
+      targets: [interact.createSnapGrid({ x: 32, y: 32 })]
+    })]    
+})
+.on('dragmove', function (event) {
+ let el=event.target
+ if(!el.dx) el.dx=el.dy=0
+ let x=parseFloat(el.dx) + event.dx,y=parseFloat(el.dy) + event.dy
+  el.dx=x,el.dy=y
+  el.style.webkitTransform =el.style.transform =`translate(${x}px,${y}px)`
+})
+}
+;
+root.draggalbe=draggable;
+})(this);
